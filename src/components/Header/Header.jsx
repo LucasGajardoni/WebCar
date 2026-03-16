a7fe49d08f74e570306e01aaad8555949a11615a
Kauã Cardoso Barbosa <kauacardosobarbosasenai@gmail.com>
2026-03-16 16:12:45 -0300

    Protótipo do header pronto
    Header quase pronto, falta apenas o ícone de lupa e pequenos ajustes.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,37 @@
+import { Link } from "react-router-dom"
+import css from "./Header.module.css"
+
 export default function Header() {
     return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img src="/Logo.png" alt="Logo" width="30" height="24"
-                         className="d-inline-block align-text-top"/>
-                    WebCar
-                </a>
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Active</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                           aria-expanded="false">Dropdown</a>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            <li>
-                                <hr className="dropdown-divider"/>
-                            </li>
-                            <li><a className="dropdown-item" href="#">Separated link</a></li>
-                        </ul>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <header className={"top-0 z-50 " + css.header}>
+            <nav className="navbar">
+                <div className="container-fluid d-flex align-items-center">
 
+                    <a className="navbar-brand d-flex align-items-center gap-2" href="#">
+                        <img src="/Logo.png" alt="Logo" width="60" height="40"/>
+                        <p className={"mt-2 " + css.azul }>Web<span className={css.cinza }>Car</span></p>
+                    </a>
+
+                    <form className="d-flex mx-4 flex-grow-1">
+                        <input
+                            className="form-control"
+                            type="search"
+                            placeholder="Buscar veículos..."
+                            />
+                    </form>
+
+                    <div className="d-flex align-items-center gap-3">
+                        <a className="nav-link" href="#">Comprar</a>
+                        <a className="nav-link" href="#">Sobre nós</a>
+                        <a className="nav-link" href="#">Entrar</a>
+
+                        <Link className={"btn btn-primary " + css.corFundo} to="/Cadastro">
+                            Cadastrar
+                        </Link>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
     )
 }
