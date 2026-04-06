0ca9a95d8239f2b98e4c1a74c7c2c2caf5027448
Kauã C <kauacardosobarbosasenai@gmail.com>
2026-04-06 11:17:01 -0300

    Arrumando erros de arquivos antigos
    Foi atualizados arquivos antigos para os atuais.

---
+++ src/components/RotaProtegida.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+
+export default function RotaProtegida(props) {
+    const usuarioId = localStorage.getItem("usuario_id");
+    const tipoUsuario = localStorage.getItem("usuario_tipo");
+
+    if (!usuarioId) {
+        return <Navigate to="/login" />;
+    }
+
+    if (tipoUsuario !== props.tipoPermitido) {
+        return <Navigate to="/login" />;
+    }
+
+    return props.componente;
+}
